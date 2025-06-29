41d026f0de6480fbbc32fb498988f145487ccbef
magdiperper12 <162820515+magdiperper12@users.noreply.github.com>
2025-06-29 23:21:32 +0300

    fix: correct typo in video tag fallback message in TV component

--- src/components/TV.tsx
+++ src/components/TV.tsx
@@ -22,7 +22,7 @@ const TV = () => {
 					src='/image/kaba.mp4'
 					type='video/mp4'
 				/>
-				Your browser does not support the video tag.
+				Your browser doesnt support the video tag.
 			</video>
 		</div>
 	);
